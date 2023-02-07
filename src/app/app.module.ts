b6bda97884a757d5be581cd4aff94a99df84d985
muhammedsaamy <muhammedsaamy@gmail.com>
2023-02-07 22:05:49 +0200

    generating component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './sharedPage/navbar/navbar.component';
+import { FooterComponent } from './sharedPage/footer/footer.component';
+import { HomeComponent } from './pages/home/home.component';
+import { MenuComponent } from './pages/menu/menu.component';
+import { AbotComponent } from './pages/abot/abot.component';
+import { AboutComponent } from './pages/about/about.component';
+import { ContactComponent } from './pages/contact/contact.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    FooterComponent,
+    HomeComponent,
+    MenuComponent,
+    AbotComponent,
+    AboutComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
