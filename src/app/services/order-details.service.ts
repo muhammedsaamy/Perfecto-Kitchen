71f5d58b3766ef288c6ebf3b8a591f861b2ba8f2
muhammedsaamy <muhammedsaamy@gmail.com>
2023-02-09 18:56:43 +0200

    adding orderDetails service and home elements

---
+++ src/app/services/order-details.service.ts
@@ -0,0 +1,51 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class OrderDetailsService {
+
+  constructor() { }
+
+  foodDetails=[
+
+    {
+      id:1,
+      foodName: 'Pancake With Sliced Strawberry',
+      foodPrice: 200,
+      foodImage:'https://images.pexels.com/photos/376464/pexels-photo-376464.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+    },
+    {
+      id:2,
+      foodName: 'Pancake With Sliced Strawberry',
+      foodPrice: 200,
+      foodImage:'https://images.pexels.com/photos/376464/pexels-photo-376464.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+    },
+    {
+      id:3,
+      foodName: 'Pancake With Sliced Strawberry',
+      foodPrice: 200,
+      foodImage:'https://images.pexels.com/photos/376464/pexels-photo-376464.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+    },
+    {
+      id:4,
+      foodName: 'Pancake With Sliced Strawberry',
+      foodPrice: 200,
+      foodImage:'https://images.pexels.com/photos/376464/pexels-photo-376464.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+    },
+    {
+      id:5,
+      foodName: 'Pancake With Sliced Strawberry',
+      foodPrice: 200,
+      foodImage:'https://images.pexels.com/photos/376464/pexels-photo-376464.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+    },
+    {
+      id:6,
+      foodName: 'Pancake With Sliced Strawberry',
+      foodPrice: 200,
+      foodImage:'https://images.pexels.com/photos/376464/pexels-photo-376464.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+    }
+
+]
+
+}
